13235a41276851cd6ac10170bb55a38569a79668
Jamal Amar Bahena <jamal@Jamals-MacBook-Pro.local>
2019-01-04 12:31:21 -0600

    cart.html not in use anymore, created a Modal for the cart, jQuery to push items to modal in process

---
+++ app.js
@@ -0,0 +1,49 @@
+
+console.log("hi");
+
+let item1 = $('#item1');
+let item2 = $('#item2');
+let item3 = $('#item3');
+let item4 = $('#item4');
+let item5 = $('#item5');
+let item6 = $('#item6');
+let item7 = $('#item7');
+let item8 = $('#item8');
+let item9 = $('#item9');
+
+
+$('#addbutton1').click(function() {
+	 $('.modal-body').append(item1)	
+});
+
+$('#addbutton2').click(function() {
+	 $('.modal-body').append(item2)	
+});
+
+$('#addbutton3').click(function() {
+	 $('.modal-body').append(item3)	
+});
+
+$('#addbutton4').click(function() {
+	 $('.modal-body').append(item4)	
+});
+
+$('#addbutton5').click(function() {
+	 $('.modal-body').append(item5)	
+});
+
+$('#addbutton6').click(function() {
+	 $('.modal-body').append(item6)	
+});
+
+$('#addbutton7').click(function() {
+	 $('.modal-body').append(item7)	
+});
+
+$('#addbutton8').click(function() {
+	 $('.modal-body').append(item8)	
+});
+
+$('#addbutton9').click(function() {
+	 $('.modal-body').append(item9)	
+});
